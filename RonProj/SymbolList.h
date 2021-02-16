60e31048c346d5719c293f0f818381b5fe32fcc5
Ron Nuriel <ron@nuriell.com>
2021-02-16 19:48:44 +0200

    need to finish SymbolList.h page 36~

--- RonProj/SymbolList.h
+++ RonProj/SymbolList.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef SYMBOL_LIST_H
+#define SYMBOL_LIST_H
+
+
+typedef enum
+{
+	CODE = 0x1,
+	DATA = (0x1 << 1),
+	EXTERNAL = (0x1 << 2),
+	ENTRY = (0x1 << 3)
+
+} SymbolAttributesEnum;
+
+
+
+typedef struct {
+	char* name;
+	int value;
+	int attributes;
+} SymbolListColumn;
+
+List* initList();
+#endif
